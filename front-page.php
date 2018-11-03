0640ad65394496306fac5a2c981747e861dc0981
OneMohrTime <derek@onemohrti.me>
2018-11-03 12:25:12 -0400

    Front page added

---
+++ front-page.php
@@ -0,0 +1,26 @@
+<?php
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			
+		<?php
+		// HOME PAGE CONTENT/CODE HERE
+		// LOOP STORED BELOW IF NEEDED
+		?>
+		
+		<?php
+		while ( have_posts() ) :
+			the_post();
+			
+			get_template_part( 'template-parts/content', 'page' );
+			
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
